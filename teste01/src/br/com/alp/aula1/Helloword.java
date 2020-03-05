693db5bf3cd9e34f66edd4cac4a9ac8a2f160031
apelicho <apelicho@BIO-01139.bio.sp>
2020-03-04 22:32:42 -0300

    atualização

---
+++ teste01/src/br/com/alp/aula1/Helloword.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package br.com.alp.aula1;
+
+/**
+ * @author apelicho
+ *
+ */
+public class Helloword {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Ola Mundo");
+		// TODO Auto-generated method stub
+	}
+
+}
